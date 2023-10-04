9a095fffff957279a36594f91a38f362fbe6bb8f
Jinnmidnnight <vkstk0131@yonsei.ac.kr>
2023-10-04 21:51:20 +0900

    wee4 수정

--- week4_2018121022.py
+++ week4_2018121022.py
@@ -2,31 +2,16 @@
 # Student ID: 2018121022
 # Assignment: Week 4
 
-# Node: 마을
 class Node:
     def __init__(self, master):
-        # 마을 주인
         self.master = master
-        
-        # 다음 마을
         self.next = None
 
-# 마을 목록: build, move 함수를 통해 마을 추가 및 개척자 이동
 class Linkedlist:
     def __init__(self, member_num):
-        # 총 개척자 수
         self.member_num = member_num
-        
-        # 출발지: 마을 주인 없기때문에 master는 None
         self.head = Node(None)
-        
-        # 개척자가 현재 있는 위치
-        # 개척자가 출발지에만 있으면 dict안에 추가되지 않음
-        # build나 move를 통해 개척자가 다른 마을로 이동하면 dict에 추가
-        # ex. {0: Node(0), 1: Node(1), 2: Node(0), 3: Node(1) ... }
         self.member_dict = {}
-        
-        # 총 마을 수
         self.size = 0
     
     def build(self, member):
